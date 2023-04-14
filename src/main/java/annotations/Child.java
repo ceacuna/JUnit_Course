1a9f6733efd166f7c2e0cd16173b1b4b4460f2c0
ceacuna <ceacuna@copyright.com>
2023-04-14 14:20:41 +0200

    Last commit before going back home

---
+++ src/main/java/annotations/Child.java
@@ -0,0 +1,10 @@
+package annotations;
+
+public class Child extends Parent{
+
+    @Override
+    public void someMethod(){
+        System.out.println("Overriding method from the parent class");
+
+    }
+}
